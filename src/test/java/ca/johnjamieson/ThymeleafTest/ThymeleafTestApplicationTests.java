ad38f91b908c19ac6fb6d0d0a1a0bd1e25ee4275
John Jamieson <CrusaderJohn@JohnJamieson.ca>
2024-07-24 20:44:09 -0400

    Thymeleaf Test

---
+++ src/test/java/ca/johnjamieson/ThymeleafTest/ThymeleafTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.johnjamieson.ThymeleafTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
